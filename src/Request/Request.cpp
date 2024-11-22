c2b1c1959dd3cb91b2a46989b5cec8968b27b6af
Kfir15 <kfireitan115@gmail.com>
2024-11-22 12:55:02 +0200

    small changed for merge.

--- src/Request/Request.cpp
+++ src/Request/Request.cpp
@@ -1,15 +1,17 @@
 #include "Request.h"
 #include "HelpRequest.h"
 #include "InvalidRequest.h"
-#include "StateManager.h"
+#include "../app/StateManager.h"
 #include <map>
+
+using namespace app;
 
 Request* Request::fromName(const string& reqName, vector<string> args) {
     // get the state manager
     auto manager = StateManager::getInstance();
 
     // get the map
-    auto map = manager->getMap();
+    auto map = manager->getRequestMap();
 
     // if he didnt find the request
     if (map.find(reqName) == map.end())  {

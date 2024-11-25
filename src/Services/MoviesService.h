3747be487fb5e6c9f3ff2eca24f200414f7b86d3
RavHaloof <avi@brandale.co.il>
2024-11-25 12:51:35 +0200

    Np 5 movies service (#8)
    * Implemented most of the MoviesService singleton, also added comparison functions, not finished with the algorithm yet
    
    * Finished recommendation algorithm for MoviesService.
    
    * Fixed minor spelling error
    Note that I have not conducted tests yet since I don't have access to Database, need to merge with main
    
    * Fixed the algorithm, added comments (Hemi we love you)
    And made some functions private
    
    * Fixed the algorithm, added comments (Hemi we love you)
    And made some functions private

---
+++ src/Services/MoviesService.h
@@ -0,0 +1,78 @@
+//
+// Created by avi on 11/22/2024.
+//
+
+#ifndef PROJECT_NETFLIX_MOVIESSERVICE_H
+#define PROJECT_NETFLIX_MOVIESSERVICE_H
+#include "../database/Database.h"
+#include "../app/StateManager.h"
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace db;
+using namespace std;
+using namespace app;
+
+class MoviesService {
+public:
+    const int MOVIE_RECOMMENDATION_COUNT = 10;
+// Delete copy constructor and assignment operator to ensure a single instance
+    MoviesService(const MoviesService &) = delete;
+    MoviesService &operator=(const MoviesService &) = delete;
+
+    /**
+     * Returns the single MovieService instance, makes sure there is only one instance existing
+     * at all times, Static method to provide global access
+     * @return StateManager
+     */
+    static MoviesService *getInstance();
+
+    /**
+     * Adds a movie to a user's watched list
+     * @param userID The user
+     * @param movieID The movie
+     */
+    void addMovieToUser(int userID, int movieID);
+
+    /**
+     *  The original movie-recommendation algorithm of Nakniki-Netflix!
+     * Recommends movies to users based on their shared watched movies with other like minded users
+     * @param userID String, the user getting the recommend
+     * @param movieID String, movie, the movie the user wishes to inspect
+     * @return  Vector<string>, returns a string of up to 10 recommended movies in order
+     */
+    vector<int> recommendMovies(int userID, int movieID);
+
+private:
+    /**
+     * Returns all the other users that watched the same movie as the given movie, and are not the user
+     * @param userID int, The userID we want to compare them to
+     * @param movieID int, The movieID we check if they watched
+     * @return vector<int>
+     */
+    vector<int> getComparableUsers(int userID, int movieID);
+
+    /**
+     * Ranks the users's likeness to userID, they get a point for each common movie with the user
+     * @param userID int, The user they will be compared to
+     * @param comparedUsers, vector<int>, the rest of the users which we wish to rank
+     * @return vector<int>, a vector of all the grades of each user (not including userID
+     */
+    vector<int> rankComparableUsers(int userID, vector<int> *comparedUsers);
+
+    /**
+     * Creates the final scores of each movie in a map
+     * @param userID the original user we compare the rest to
+     * @param users vector<int> The list of users compared
+     * @param ranks vector<int> The recommendation values of each user
+     * @return map<int, int>, the map containing each movie, and their recommendation score
+     */
+    map<int, int> mapComparableMovies(int userID, vector<int> *users, vector<int> *ranks);
+
+    static MoviesService *instance;
+
+    MoviesService() = default;
+};
+
+
+#endif //PROJECT_NETFLIX_MOVIESSERVICE_H

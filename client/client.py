a8be2086c51bf22fa55fe54ca10a8d549531a5af
Roky360 <edenshkd@gmail.com>
2024-12-06 11:03:17 +0200

    Wrote client in python (needs to be tested together with the server)

---
+++ client/client.py
@@ -0,0 +1,30 @@
+import sys
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+def main():
+    if len(sys.argv) < 3:
+        print(f"Usage: {sys.argv[0]} <ip> <port>")
+        return 0
+    # get ip and port from CMD arguments
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    try: # parse the port to integer
+        port = int(port)
+    except ValueError:
+        print("Port must be an integer")
+        return 0
+
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect((ip, port))  # connect to server
+
+    # receive requests from user, send to server and wait for response
+    while True:
+        req = input()
+        sock.send(req.encode())
+        res = sock.recv(1024)
+        print(res.decode())
+
+
+if __name__ == '__main__':
+    main()

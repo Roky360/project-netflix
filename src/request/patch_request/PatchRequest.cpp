72f844a9c93ef4507a3a1da94d61827d10ed604a
Kfir15 <kfireitan115@gmail.com>
2024-12-06 23:23:43 +0200

    created PATCH request (need to add the request to the requests map)

---
+++ src/request/patch_request/PatchRequest.cpp
@@ -0,0 +1,58 @@
+#include "PatchRequest.h"
+#include "../../services/MoviesService.h"
+#include <string>
+
+PatchRequest::PatchRequest(const vector<std::string> &args, ClientContext *cl) : Request(args, cl) {}
+
+PatchRequest::PatchRequest() : Request() {}
+
+string PatchRequest::getHelpMsg() {
+    return "PATCH, arguments: [userid] [movieid1] [movieid2] ...";
+}
+
+Response* PatchRequest::execute() {
+    // get the move service instance
+    MoviesService *service = MoviesService::getInstance();
+
+    // get the database instance
+    Database* db = StateManager::getInstance()->getDb();
+
+    // if the user didnt entered enough arguments.
+    if (args.size() <= 1) {
+        return new Response(BAD_REQUEST_400, this->context);
+    }
+
+    // get the user id from the arguments
+    int userId;
+    try {
+        userId = stoi(args[0]);
+    } catch (...) {
+        return new Response(BAD_REQUEST_400, this->context);
+    }
+
+    // check if the user already exist
+    if (!db->userExist(userId)) {
+        return new Response(NOT_FOUND_404, this->context);
+    }
+
+    // enter the movies id to the vector
+    vector<int> movies;
+    for (int i = 1; i < args.size(); ++i) {
+        // try to transfer to int and then push it
+        try {
+            auto movieId = stoi(args[i]);
+            movies.push_back(movieId);
+        } catch (...) {
+            return new Response(BAD_REQUEST_400, this->context);
+        }
+    }
+
+    // add the movie to the user
+    for (auto movieID : movies) {
+        service->addMovieToUser(userId, movieID);
+    }
+
+    // return ok response
+    return new Response(OK_200, this->context);
+
+}
